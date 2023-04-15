7d472bb02571016c951216aee31b749ab5bc0f08
mojens <moha135h@stud.kea.dk>
2023-04-15 05:15:09 +0200

    app.use(express.static(public)); doesnt work

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ import PageGenerator from './util/PageGenerator.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'), {
-    setHeaders: (res, path) => {
-      const mimeType = mime.getType(path);
-      res.set('Content-Type', mimeType);
-    }
-  }));
+app.use(express.static("public"));
 
 
 
