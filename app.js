22ef2681fdfe43eb7c9fd364303482966b35ce16
mojens <moha135h@stud.kea.dk>
2023-04-15 04:03:11 +0200

    fixing vercel ?

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import TemplateEngine from './util/TemplateEngine.js';
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const frontPage = TemplateEngine.renderPage(TemplateEngine.readPage("./public/pages/frontpage/frontpage.html"), {
     tabTitle: "Frontpage",
