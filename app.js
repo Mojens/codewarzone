f881b8e7f8357b1a247d1b978a584eccf990daba
mojens <moha135h@stud.kea.dk>
2023-04-15 05:14:08 +0200

    fix ?

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import PageGenerator from './util/PageGenerator.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, path) => {
+      const mimeType = mime.getType(path);
+      res.set('Content-Type', mimeType);
+    }
+  }));
 
 
 
