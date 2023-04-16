201d3242d7ef4c299d84c08b150de016fe2686fd
mojens <moha135h@stud.kea.dk>
2023-04-16 12:35:25 +0200

    mobile responsive, now mail, openai and exercises is missing

--- public/pages/frontpage/frontpage.js
+++ public/pages/frontpage/frontpage.js
@@ -68,4 +68,74 @@ $(function() {
             break;
       }
   }
+
+  $(function() {
+    var data = [
+    { 
+      action: 'type',
+      strings: ["npm install -g codewarzone^400"],
+      output: '<span class="gray">+codewarzone@0.2.1 installed</span><br>&nbsp;',
+      postDelay: 1000
+    },
+    { 
+      action: 'type',
+      strings: ["cd codewarzone^400"],
+      output: ' ',
+      postDelay: 1000
+    },
+    { 
+      action: 'type',
+      //clear: true,
+      strings: ['run codewarzone^400'],
+      output: $('.mimik-run-output').html()
+    },
+    { 
+      action: 'type',
+      strings: ["Lets Goo!!", ''],
+      postDelay: 2000
+    }
+    
+  ];
+    runScriptsMobile(data, 0);
+  });
   
+  function runScriptsMobile(data, pos) {
+    var prompt = $('.promptMobile'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.historyMobile').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor-mobile').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 30,
+            callback: function() {
+              var history = $('.historyMobile').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.historyMobile').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminalMobile').scrollTop($('section.terminalMobile').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScriptsMobile(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
