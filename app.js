20d21230621f74782827538af13863f66a73f579
mojens <moha135h@stud.kea.dk>
2023-04-16 03:08:23 +0200

    fix css vercel

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import PageGenerator from './util/PageGenerator.js';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(path.resolve(), '/public')));
 
 
 
