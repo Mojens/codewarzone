a1340972aa479a4a1cc79a3d29aab44b058d00c7
mojens <moha135h@stud.kea.dk>
2023-04-17 09:45:20 +0200

    fix js

--- public/pages/exercises/editor.js
+++ public/pages/exercises/editor.js
@@ -1,15 +1,21 @@
 const editorElement = document.getElementById("editor");
-let editor = ace.edit(editorElement)
-editor.setTheme("ace/theme/cobalt")
-editor.session.setMode("ace/mode/javascript")
+let editor = ace.edit(editorElement);
+editor.setTheme("ace/theme/cobalt");
+editor.session.setMode("ace/mode/javascript");
 editor.setOptions({
-    fontSize: "16pt",
     showLineNumbers: true,
     showGutter: true,
     vScrollBarAlwaysVisible: true,
     enableBasicAutocompletion: true,
     enableLiveAutocompletion: true
-})
+});
 
+// Check if the screen width is less than or equal to 768 pixels (the width of most tablets and mobile devices)
+if (window.innerWidth <= 768) {
+    editor.setOption("fontSize", "12pt"); // set font size to 14pt on mobile devices
+} else {
+    editor.setOption("fontSize", "16pt"); // set font size to 16pt on desktop devices
+}
 
-export { editor }
+export { editor };
+
