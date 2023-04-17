c1373618694b99c814944289be195b4fd0cc8b1a
mojens <moha135h@stud.kea.dk>
2023-04-17 09:21:01 +0200

    designing exercise pages

--- public/pages/exercises/editor.js
+++ public/pages/exercises/editor.js
@@ -2,5 +2,14 @@ const editorElement = document.getElementById("editor");
 let editor = ace.edit(editorElement)
 editor.setTheme("ace/theme/cobalt")
 editor.session.setMode("ace/mode/javascript")
+editor.setOptions({
+    fontSize: "16pt",
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible: true,
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true
+})
+
 
 export { editor }
--- public/pages/exercises/handleQuestionAndAnswer.js
+++ public/pages/exercises/handleQuestionAndAnswer.js
@@ -1,4 +1,23 @@
 import { editor } from "./editor.js";
+document.getElementById("run-code").addEventListener("click", () => {
+     let code = editor.getValue();
+
+     let output = '';
+     let originalLog = console.log;
+     console.log = function(value) {
+         output += value + '\n';
+         originalLog.apply(console, arguments);
+     };
+
+     try {
+         eval(code);
+         document.getElementById('output').textContent = output;
+     } catch (err) {
+         document.getElementById('output').textContent = err.message;
+     }
+
+     console.log = originalLog;
+});
 
 let questionElement = document.getElementById("question")
 let answer = editor.getValue()
